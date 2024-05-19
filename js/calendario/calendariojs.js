5017db0524c097e0ed237a662dea79cc33d1b4d5
Hakeem <126789780+HoopStar23@users.noreply.github.com>
2024-05-19 11:06:02 +0200

    calendario hecho

---
+++ js/calendario/calendariojs.js
@@ -0,0 +1,64 @@
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".dias"),
+prevNextIcon = document.querySelectorAll(".icons span");
+
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+const month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+const renderCalendar = () => {
+    let firstDate = new Date(currYear, currMonth + 1 ,1).getDay(),
+    lastDate = new Date(currYear, currMonth + 1 ,0).getDate(),
+    lastDayMonth = new Date(currYear, currMonth,lastDate).getDay(),
+    lastDateofMonth = new Date(currYear, currMonth,0).getDate();
+
+    let liTag = "";
+
+    for (let i = firstDate; i > 0; i--) {
+        liTag += `<li class="inactivo">${lastDateofMonth - i + 1}</li>`
+    }
+
+    for (let i = 1; i <= lastDate; i++) {
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+                            && currYear === new Date().getFullYear() ? "activo" : "";
+        liTag += `<li class="${isToday}">${i}</li>`
+    }
+
+    for (let i = lastDayMonth; i < 6; i++) {
+        liTag += `<li class="inactivo">${i - lastDayMonth + 1}</li>`
+    }
+    currentDate.innerText = `${month[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+
+    document.querySelectorAll(".dias li:not(.inactivo)").forEach(day => {
+        day.addEventListener("click", (e) => {
+            document.querySelectorAll(".dias li.evento").forEach(selectedDay => {
+                selectedDay.classList.remove("evento");
+            });
+            e.target.classList.add("evento");
+        });
+    });
+}
+
+renderCalendar();
+
+prevNextIcon.forEach(icon =>{
+    icon.addEventListener("click", () =>{
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        if (currMonth < 0|| currMonth > 11) {
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        }else{
+            date = new Date();
+        }
+        renderCalendar();
+    })
+})
+
+function marcarEvento(){
+
+}
